f03a9dbc20f27b7d117f73711709cd53151a4a45
regenbogencode <regenbogencode@gmail.com>
2019-02-25 14:01:56 +0100

    made examples prettier

--- examples/sender.ino
+++ examples/sender.ino
@@ -1,8 +1,8 @@
-/* 
+/*
  * "THE BEER-WARE LICENSE" (Revision 42):
- * regenbogencode@gmail.com wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return
+ * regenbogencode@gmail.com wrote this file. As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you
+ * think this stuff is worth it, you can buy me a beer in return
  */
 #include <Arduino.h>
 #ifdef ESP8266
@@ -11,7 +11,9 @@
 #include <WiFi.h>
 #endif
 #include "ESPNowW.h"
+
 uint8_t receiver_mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
+
 void setup() {
     Serial.begin(115200);
     Serial.println("ESPNow sender Demo");
